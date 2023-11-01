dd41ad80aed4ecd113a29a1ee6b6a9134985093e
v0xie <28695009+v0xie@users.noreply.github.com>
2023-10-31 17:01:27 -0700

    citation, don't save gui parameters

--- scripts/cads.py
+++ scripts/cads.py
@@ -4,99 +4,129 @@ import numpy as np
 
 from modules import script_callbacks
 from modules.script_callbacks import CFGDenoiserParams
-from modules.processing import process_images
 
 import torch
 
-class ExtensionTemplateScript(scripts.Script):
+"""
+
+An implementation of CADS: Unleashing the Diversity of Diffusion Models through Condition-Annealed Sampling for Automatic1111 Webui
+
+@misc{sadat2023cads,
+      title={CADS: Unleashing the Diversity of Diffusion Models through Condition-Annealed Sampling},
+      author={Seyedmorteza Sadat and Jakob Buhmann and Derek Bradely and Otmar Hilliges and Romann M. Weber},
+      year={2023},
+      eprint={2310.17347},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+
+Author: v0xie
+GitHub URL: https://github.com/v0xie/sd-webui-cads
+
+"""
+class CADSExtensionScript(scripts.Script):
         # Extension title in menu UI
         def title(self):
-                return "Extension Template"
+                return "CADS"
 
         # Decide to show menu in txt2img or img2img
-        # - in "txt2img" -> is_img2img is `False`
-        # - in "img2img" -> is_img2img is `True`
-        #
-        # below code always show extension menu
         def show(self, is_img2img):
                 return scripts.AlwaysVisible
 
         # Setup menu ui detail
         def ui(self, is_img2img):
-                with gr.Accordion('Extension Template', open=False):
+                with gr.Accordion('CADS', open=False):
+                        active = gr.Checkbox(value=False, default=False, label="Active", elem_id='cads_active')
+                        rescale = gr.Checkbox(value=True, default=True, label="Rescale CFG", elem_id = 'cads_rescale')
                         with gr.Row():
-                                angle = gr.Slider(
-                                        minimum=0.0,
-                                        maximum=360.0,
-                                        step=1,
-                                        value=0,
-                                        label="Angle"
-                                )
-                                checkbox = gr.Checkbox(
-                                        False,
-                                        label="Checkbox"
-                                )
-                # TODO: add more UI components (cf. https://gradio.app/docs/#components)
-                return [angle, checkbox]
+                                t1 = gr.Slider(value = 0.6, minimum = 0.0, maximum = 1.0, step = 0.05, label="Tau 1", elem_id = 'cads_tau1', info="Step to start interpolating from full strength. Default 0.6")
+                                t2 = gr.Slider(value = 0.9, minimum = 0.0, maximum = 1.0, step = 0.05, label="Tau 2", elem_id = 'cads_tau2', info="Step to stop affecting image. Default 0.9")
+                        with gr.Row():
+                                noise_scale = gr.Slider(value = 0.25, minimum = 0.0, maximum = 1.0, step = 0.01, label="Noise Scale", elem_id = 'cads_noise_scale', info='Scale of noise injected at every time step, default 0.25, recommended <= 0.3')
+                                mixing_factor= gr.Slider(value = 1.0, minimum = 0.0, maximum = 1.0, step = 0.01, label="Mixing Factor", elem_id = 'cads_mixing_factor', info='Regularization factor, lowering this will increase the diversity of the images with more chance of divergence, default 1.0')
+                active.do_not_save_to_config = True
+                rescale.do_not_save_to_config = True
+                t1.do_not_save_to_config = True
+                t2.do_not_save_to_config = True
+                noise_scale.do_not_save_to_config = True
+                mixing_factor.do_not_save_to_config = True
+                return [active, t1, t2, noise_scale, mixing_factor, rescale]
 
-        # Extension main process
-        # Type: (StableDiffusionProcessing, List<UI>) -> (Processed)
-        # args is [StableDiffusionProcessing, UI1, UI2, ...]
-        def run(self, p, angle, checkbox):
-                # TODO: get UI info through UI object angle, checkbox
-                proc = process_images(p)
-                # TODO: add image edit process via Processed object proc
-                return proc
+        def before_process(self, p, active, t1, t2, noise_scale, mixing_factor, rescale, *args, **kwargs):
+                active = getattr(p, "cads_active", active)
+                if active is False:
+                        return
+                t1 = getattr(p, "cads_tau1", t1)
+                t2 = getattr(p, "cads_tau2", t2)
+                noise_scale = getattr(p, "cads_noise_scale", noise_scale)
+                mixing_factor = getattr(p, "cads_mixing_factor", mixing_factor)
+                rescale = getattr(p, "cads_rescale", rescale)
 
-def cads_linear_schedule(t, tau1, tau2):
-        """ CADS annealing schedule function """
-        if t <= tau1:
-                return 1.0
-        if t>= tau2:
-                return 0.0
-        gamma = (tau2-t)/(tau2-tau1)
-        return gamma
+                p.extra_generation_params = {
+                        "CADS Active": active,
+                        "CADS Tau 1": t1,
+                        "CADS Tau 2": t2,
+                        "CADS Noise Scale": noise_scale,
+                        "CADS Mixing Factor": mixing_factor,
+                        "CADS Rescale": rescale,
+                }
 
-def add_noise(y, gamma, noise_scale, psi, rescale=False):
-        """ CADS adding noise to the condition
+                # Use lambda to call the callback function with the parameters to avoid global variables
+                y = lambda params: self.on_cfg_denoiser_callback(params, t1=t1, t2=t2, noise_scale=noise_scale, mixing_factor=mixing_factor, rescale=rescale)
 
-        Arguments:
-        y: Input condition
-        gamma: Noise level
-        noise_scale: Noise scale
-        psi: Rescaling factor
-        rescale (bool): Rescale the condition
+                # print('Hooked into callbacks')
+                script_callbacks.on_cfg_denoiser(y)
+                script_callbacks.on_script_unloaded(self.unhook_callbacks)
 
+        def postprocess_batch(self, p, active, t1, t2, noise_scale, mixing_factor, rescale, *args, **kwargs):
+                self.unhook_callbacks()
+                # if kwargs.get('active', False) is False or not hasattr(p.extra_generation_params, 'CADS Active'):
+                #         return
 
-        """
-        y_mean, y_std = torch.mean(y), torch.std(y)
-        y = np.sqrt(gamma) * y + noise_scale * np.sqrt(1-gamma) * torch.randn_like(y)
-        if rescale:
-                y_scaled = (y - torch.mean(y)) / torch.std(y) * y_std + y_mean
-                if not torch.isnan(y_scaled).any():
-                        y = psi * y_scaled + (1 - psi) * y
-                else:
-                        print("Warning: NaN encountered in rescaling")
-        return y
+        def unhook_callbacks(self):
+                #print('Unhooked callbacks')
+                script_callbacks.remove_current_script_callbacks()
 
-def on_cfg_denoiser_callback(params: CFGDenoiserParams):
-        print("cads")
-        sampling_step = params.sampling_step
-        total_sampling_step = params.total_sampling_steps
-        text_cond = params.text_cond
-        text_uncond = params.text_uncond
+        def cads_linear_schedule(self, t, tau1, tau2):
+                """ CADS annealing schedule function """
+                if t <= tau1:
+                        return 1.0
+                if t>= tau2:
+                        return 0.0
+                gamma = (tau2-t)/(tau2-tau1)
+                return gamma
 
-        initial_noise_scale  = 0.1      # s
-        t1 = 0.2          # tau1
-        t2 = 0.9          # tau2 - cutoff
-        mixing_factor = 1.0             # ψ
-        rescale = False
+        def add_noise(self, y, gamma, noise_scale, psi, rescale=False):
+                """ CADS adding noise to the condition
 
-        t = max(min(sampling_step / total_sampling_step, 1.0), 0.0)
-        gamma = cads_linear_schedule(t, t1, t2)
-        params.text_cond['crossattn'] = add_noise(text_cond['crossattn'], gamma, initial_noise_scale, mixing_factor, rescale)
-        params.text_uncond['crossattn'] = add_noise(text_uncond['crossattn'], gamma, initial_noise_scale, mixing_factor, rescale)
-        params.text_cond['vector'] = add_noise(text_cond['vector'], gamma, initial_noise_scale, mixing_factor, rescale)
-        params.text_uncond['vector'] = add_noise(text_uncond['vector'], gamma, initial_noise_scale, mixing_factor, rescale)
+                Arguments:
+                y: Input conditioning
+                gamma: Noise level w.r.t t
+                noise_scale (float): Noise scale
+                psi (float): Rescaling factor
+                rescale (bool): Rescale the condition
+                """
+                y_mean, y_std = torch.mean(y), torch.std(y)
+                y = np.sqrt(gamma) * y + noise_scale * np.sqrt(1-gamma) * torch.randn_like(y)
+                if rescale:
+                        y_scaled = (y - torch.mean(y)) / torch.std(y) * y_std + y_mean
+                        if not torch.isnan(y_scaled).any():
+                                y = psi * y_scaled + (1 - psi) * y
+                        else:
+                                print("Warning: NaN encountered in rescaling")
+                return y
 
-script_callbacks.on_cfg_denoiser(on_cfg_denoiser_callback)
+        def on_cfg_denoiser_callback(self, params: CFGDenoiserParams, t1, t2, noise_scale, mixing_factor, rescale):
+                sampling_step = params.sampling_step
+                total_sampling_step = params.total_sampling_steps
+                text_cond = params.text_cond
+                text_uncond = params.text_uncond
+
+                t = 1.0 - max(min(sampling_step / total_sampling_step, 1.0), 0.0) # Algorithms assumes we start at 1.0 and go to 0.0
+                gamma = self.cads_linear_schedule(t, t1, t2)
+
+                params.text_cond['crossattn'] = self.add_noise(text_cond['crossattn'], gamma, noise_scale, mixing_factor, rescale)
+                params.text_uncond['crossattn'] = self.add_noise(text_uncond['crossattn'], gamma, noise_scale, mixing_factor, rescale)
+                params.text_cond['vector'] = self.add_noise(text_cond['vector'], gamma, noise_scale, mixing_factor, rescale)
+                params.text_uncond['vector'] = self.add_noise(text_uncond['vector'], gamma, noise_scale, mixing_factor, rescale)
+
